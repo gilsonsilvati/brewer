863e39fc30583208de6ff56de6509b151595a532
Gilson Silva <gilsonsilvati@gmail.com>
2017-09-24 19:40:40 -0300

    Começando o cadastro de cerveja

--- src/main/java/br/com/brewer/controller/CervejasController.java
+++ src/main/java/br/com/brewer/controller/CervejasController.java
@@ -2,6 +2,9 @@ package br.com.brewer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.brewer.model.Cerveja;
 
 @Controller
 @RequestMapping("/cervejas")
@@ -9,6 +12,14 @@ public class CervejasController {
 	
 	@RequestMapping("/novo")
 	public String novo() {
+		return "cerveja/CadastroCerveja";
+	}
+	
+	@RequestMapping(value = "/novo", method = RequestMethod.POST)
+	public String cadastrar(Cerveja cerveja) {
+		System.out.println(">>>> sku: " + cerveja.getSku());
+		System.out.println(">>>> nome: " + cerveja.getNome());
+		
 		return "cerveja/CadastroCerveja";
 	}
 
