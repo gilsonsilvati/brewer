35aa31b1b9d800d2339b7676a7b5a683d09d8b01
Gilson Silva <gilsonsilvati@gmail.com>
2017-09-26 21:51:36 -0300

    Validando outros campos.

--- src/main/java/br/com/brewer/model/Cerveja.java
+++ src/main/java/br/com/brewer/model/Cerveja.java
@@ -1,17 +1,39 @@
 package br.com.brewer.model;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class Cerveja {
-	
+
 	@NotBlank
 	private String sku;
-	
+
+	@NotBlank
+	private String nome;
+
+	@NotBlank
+	@Size(min = 1, max = 50)
+	private String descricao;
+
 	public String getSku() {
 		return sku;
 	}
 	public void setSku(String sku) {
 		this.sku = sku;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 
 }
