ed1764e98472141a0c6a3d4e81e62f3d1ec27646
Gilson Silva <gilsonsilvati@gmail.com>
2017-09-28 00:46:26 -0300

    Baixando e configurando o layout no projeto.

--- src/main/java/br/com/brewer/controller/CervejasController.java
+++ src/main/java/br/com/brewer/controller/CervejasController.java
@@ -30,5 +30,10 @@ public class CervejasController {
 		System.out.println(">>>> sku: " + cerveja.getSku());
 		return "redirect:/cervejas/novo";
 	}
+	
+	@RequestMapping("/cadastro")
+	public String cadastro() {
+		return "cerveja/cadastro-produto";
+	}
 
 }
