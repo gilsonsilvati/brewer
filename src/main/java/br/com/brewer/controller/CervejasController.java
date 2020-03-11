18583b2dbfb96578c453638d58ba4238d0b70968
Gilson Silva <gilsonsilvati@gmail.com>
2020-03-10 23:13:08 -0300

    Configurando o projeto: JPA e Data Source

--- src/main/java/br/com/brewer/controller/CervejasController.java
+++ src/main/java/br/com/brewer/controller/CervejasController.java
@@ -2,6 +2,7 @@ package br.com.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,13 +11,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.brewer.model.Cerveja;
+import br.com.brewer.repository.Cervejas;
 
 @Controller
 @RequestMapping("/cervejas")
 public class CervejasController {
 	
+	@Autowired
+	private Cervejas cervejas;
+	
 	@RequestMapping("/novo")
 	public String novo(Cerveja cerveja) {
+		cervejas.findAll();
+		
 		return "cerveja/CadastroCerveja";
 	}
 	
