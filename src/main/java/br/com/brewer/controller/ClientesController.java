f60ea8b232d7d62869de0910e363bdf74ec34bd5
Gilson Silva <gilsonsilvati@gmail.com>
2020-08-11 23:10:25 -0300

    Desafio: Começando a pesquisa de clientes

--- src/main/java/br/com/brewer/controller/ClientesController.java
+++ src/main/java/br/com/brewer/controller/ClientesController.java
@@ -1,18 +1,25 @@
 package br.com.brewer.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.com.brewer.controller.page.PageWrapper;
 import br.com.brewer.model.Cliente;
 import br.com.brewer.model.enumerations.TipoPessoa;
+import br.com.brewer.repository.Clientes;
 import br.com.brewer.repository.Estados;
+import br.com.brewer.repository.filter.ClienteFilter;
 import br.com.brewer.service.CadastroClienteService;
 import br.com.brewer.service.exception.CpfCnpjJaCadastradoException;
 
@@ -25,6 +32,9 @@ public class ClientesController {
 	
 	@Autowired
 	private CadastroClienteService cadastroClienteService;
+	
+	@Autowired
+	private Clientes clientes;
 	
 	@RequestMapping("novo")
 	public ModelAndView novo(Cliente cliente) {
@@ -50,6 +60,17 @@ public class ClientesController {
 		}
 		
 		return new ModelAndView("redirect:/clientes/novo");
+	}
+	
+	@GetMapping
+	public ModelAndView pesquisar(ClienteFilter clienteFilter, BindingResult result, @PageableDefault(size = 3) Pageable pageable, HttpServletRequest httpServletRequest) {
+		ModelAndView mv = new ModelAndView("cliente/PesquisaClientes");
+		mv.addObject("tiposPessoa", TipoPessoa.values());
+		
+		PageWrapper<Cliente> paginaWrapper = new PageWrapper<>(clientes.filtrar(clienteFilter, pageable), httpServletRequest);
+		mv.addObject("pagina", paginaWrapper);
+		
+		return mv;
 	}
 
 }
--- src/main/java/br/com/brewer/repository/Clientes.java
+++ src/main/java/br/com/brewer/repository/Clientes.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.brewer.model.Cliente;
+import br.com.brewer.repository.helper.cliente.ClientesQueries;
 
 @Repository
-public interface Clientes extends JpaRepository<Cliente, Long> {
+public interface Clientes extends JpaRepository<Cliente, Long>, ClientesQueries {
 	
 	public Optional<Cliente> findByNomeIgnoreCase(String nome);
 
