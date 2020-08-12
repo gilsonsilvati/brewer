3c9df61489be610f8c7354e9145c00e90c756358
Gilson Silva <gilsonsilvati@gmail.com>
2020-08-12 16:12:30 -0300

    Desafio: Finalizar cadastro de cidade

--- src/main/java/br/com/brewer/repository/Cidades.java
+++ src/main/java/br/com/brewer/repository/Cidades.java
@@ -1,15 +1,19 @@
 package br.com.brewer.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.brewer.model.Cidade;
+import br.com.brewer.model.Estado;
 
 @Repository
 public interface Cidades extends JpaRepository<Cidade, Long> {
 
 	public List<Cidade> findByEstadoCodigo(Long codigoEstado);
+	
+	public Optional<Cidade> findByNomeAndEstado(String nome, Estado estado);
 
 }
