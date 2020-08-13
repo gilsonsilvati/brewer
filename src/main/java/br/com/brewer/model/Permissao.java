7030b71e3cd18131a6a58d4b1c5ad3105c5535a4
Gilson Silva <gilsonsilvati@gmail.com>
2020-08-13 17:01:49 -0300

    Criando a migração do usuário, grupo e permissão

---
+++ src/main/java/br/com/brewer/model/Permissao.java
@@ -0,0 +1,57 @@
+package br.com.brewer.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
+@Entity
+@Table(name = "permissao")
+public class Permissao implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	private String nome;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		
+		if ((obj == null) || (getClass() != obj.getClass()))
+			return false;
+		
+		Permissao other = (Permissao) obj;
+		return new EqualsBuilder().append(codigo, other.codigo).isEquals();
+	}
+
+}
