cba3a3ca255488b4918f7675d5aac4c3032922ba
Gilson Silva <gilsonsilvati@gmail.com>
2020-08-29 17:13:23 -0300

    Corrigindo problema de acentuação após Spring Security

---
+++ src/main/resources/db/migration/V12__adicionar_cidade_e_estado_para_sao_paulo.sql
@@ -0,0 +1,4 @@
+INSERT INTO cidade (nome, codigo_estado) VALUES ('São Paulo', 6);
+INSERT INTO cidade (nome, codigo_estado) VALUES ('Ribeirão Preto', 6);
+INSERT INTO cidade (nome, codigo_estado) VALUES ('Campinas', 6);
+INSERT INTO cidade (nome, codigo_estado) VALUES ('Santos', 6);
