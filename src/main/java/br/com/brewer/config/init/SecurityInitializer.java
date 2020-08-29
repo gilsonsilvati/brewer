0c1cc4c855da3ada3c61e57af34140fe28d47632
Gilson Silva <gilsonsilvati@gmail.com>
2020-08-29 18:49:49 -0300

    Configurando duração da sessão

--- src/main/java/br/com/brewer/config/init/SecurityInitializer.java
+++ src/main/java/br/com/brewer/config/init/SecurityInitializer.java
@@ -1,7 +1,10 @@
 package br.com.brewer.config.init;
 
+import java.util.EnumSet;
+
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
+import javax.servlet.SessionTrackingMode;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -10,6 +13,13 @@ public class SecurityInitializer extends AbstractSecurityWebApplicationInitializ
 	
 	@Override
 	protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+		// Sessão geral do sistema, independente se estiver mexendo ou não (segundos)
+//		servletContext.getSessionCookieConfig().setMaxAge(20);
+		
+		// Removendo JSESSIONID da URL do Browser (pode ser um risco para a aplicação)
+		servletContext.setSessionTrackingModes(EnumSet.of(SessionTrackingMode.COOKIE));
+		
+		
 		FilterRegistration.Dynamic characterEncodingFilter = servletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
 		characterEncodingFilter.setInitParameter("encoding", "UTF-8");
 		characterEncodingFilter.setInitParameter("forceEncoding", "true");
